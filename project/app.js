8d18a218bfc09aa873975df94ce61ff736f72df7
MrAlexCox <afcox90@gmail.com>
2021-02-16 17:17:16 +0000

    update

--- project/app.js
+++ project/app.js
@@ -4,19 +4,21 @@
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+const cityNames = data;
+
 // get city names from JSON
 
-function cityList () {
-    var citynames = JSON.parse(data);
-    alert(citynames[0]);
+function cityList() {
+    let x = cityNames.name;
+
+    console.log(x[200]);
 } 
 
 function citySearch() {
     var flag = false;
-    cityName = document.getElementById("movieSearch").value;
-    movies = document.getElementsByTagName("h3");
-    for (var i = 0; i < movies.length; i++) {
-        if (movieName === movies[i].innerHTML) {
+    userInput = document.getElementById("userCity").value;
+    for (var i = 0; i < cityNames.length; i++) {
+        if (userInput === cityNames[i].innerHTML) {
             movies[i].style.backgroundColor = "yellow";
             flag = true;
         } else {
@@ -24,6 +26,6 @@ function citySearch() {
         }
     }
     if(flag == false) {
-        alert("no such movie was found");
+        alert("No such city was found. Please check your spelling.");
     }
 };
